c0290433e74e2010a8680a7bf05fd0e8624875bf
JonathanMoiseyev <jonathanmoiseyev@outlook.com>
2024-03-19 13:58:55 +0200

    created a wrapper for simple use of BFS

---
+++ BFS.java
@@ -0,0 +1,61 @@
+import org.jgrapht.Graph;
+
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+public class BFS<V,E> {
+    private final Graph<V,E> graph;
+    private final V start;
+
+    private final V furthest;
+
+    private final BreadthFirstIterator<V,E> iterator;
+
+//    private ShortestPathAlgorithm.SingleSourcePaths<V,E> shortestPaths;
+
+    BFS(Graph<V,E> graph, V start) {
+        this.graph = graph;
+        this.start = start;
+
+        iterator = new BreadthFirstIterator<>(graph, start);
+
+        V currentFurthest = start;
+        while (iterator.hasNext()) {
+            currentFurthest = iterator.next();
+        }
+        furthest = currentFurthest;
+
+
+//        shortestPaths = new BFSShortestPath<>(graph).getPaths(start);
+//
+//        double CurrentFurthestDepth = 0.0;
+//        V currentFurthest = start;
+//        double currentDepth;
+//        for (V v : graph.vertexSet()) {
+//            currentDepth = shortestPaths.getWeight(v);
+//            if (currentDepth > CurrentFurthestDepth) {
+//                CurrentFurthestDepth = currentDepth;
+//                currentFurthest = v;
+//            }
+//        }
+//
+//        this.furthest = currentFurthest;
+
+    }
+
+
+    public int getEcc() {
+        return iterator.getDepth(furthest);
+    }
+
+    private V getNthParent(V v, int n) {
+        V parent = v;
+        for (int i = 0; i < n; i++) {
+            parent = iterator.getParent(parent);
+        }
+    }
+
+    public V getMiddleOfEccPath() {
+        return getNthParent(furthest, getEcc() / 2);
+    }
+
+}
