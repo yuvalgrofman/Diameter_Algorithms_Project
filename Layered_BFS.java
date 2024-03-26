7cd279228fbd35977828e9a1a48516b4e80a48d1
Yuval Grofman <yuvalgrofman@gmail.com>
2024-03-26 12:33:03 +0200

    added Layered_BFS.java class

---
+++ Layered_BFS.java
@@ -0,0 +1,34 @@
+import org.jgrapht.Graph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Layered_BFS<V,E> {
+    private final Graph<V,E> graph;
+    private final V start;
+    private final BreadthFirstIterator<V,E> iterator;
+    private final Set<V> layer;
+
+    public Layered_BFS(Graph<V,E> graph, V start, int layer_depth) {
+        this.graph = graph;
+        this.start = start;
+
+        iterator = new BreadthFirstIterator<>(graph, start);
+        Set<V> temp = new HashSet<>();
+
+        V current = start;
+        while (iterator.hasNext() && iterator.getDepth(current) <= layer_depth) {
+            current = iterator.next();
+            if (iterator.getDepth(current) == layer_depth) {
+                temp.add(current);
+            }
+        }
+
+        layer = temp;
+    }
+
+    public Set<V> getLayer() {
+        return layer;
+    }
+}
