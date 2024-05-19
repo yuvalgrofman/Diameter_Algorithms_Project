74d9b344b9ed87b01e715862084f60db17a70e4e
JonathanMoiseyev <jonathanmoiseyev@outlook.com>
2024-05-19 15:08:16 +0300

    made a parser that parses a text representation of a graph, and creates a graph using that data.

---
+++ src/main/java/graphParsers/GraphFromTextParser.java
@@ -0,0 +1,36 @@
+package graphParsers;
+
+import org.jgrapht.Graph;
+
+import org.jgrapht.graph.builder.GraphBuilder;
+import specialScanner.SpecializedScanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class GraphFromTextParser<V, E, G extends Graph<V, E>> {
+    public G parse(String pathToTxtFile, G baseGraph, SpecializedScanner<V> vertexScanner) {
+        GraphBuilder<V , E, G> builder = new GraphBuilder<>(baseGraph);
+        
+        File file = new File(pathToTxtFile);
+        try(Scanner scanner = new Scanner(file)) {
+            vertexScanner.setScanner(scanner);
+
+            while (vertexScanner.hasNext()) {
+                V source = vertexScanner.next();
+
+                if (!vertexScanner.hasNext()) {
+                    throw new GraphParseException("file " + pathToTxtFile + " has incorrect format");
+                }
+
+                builder.addEdge(source, vertexScanner.next());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("incorrect file path");
+            return null;
+        }
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/graphParsers/GraphParseException.java
@@ -0,0 +1,7 @@
+package graphParsers;
+
+public class GraphParseException extends RuntimeException {
+    public GraphParseException(String s) {
+        super(s);
+    }
+}
