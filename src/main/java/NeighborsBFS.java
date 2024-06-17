86a4629e09fc82dcb9610e160032262c26415d27
Yuval Grofman <yuvalgrofman@gmail.com>
2024-06-17 11:02:32 +0300

    Added NeighborsBFS which find s closest vertices

---
+++ src/main/java/NeighborsBFS.java
@@ -0,0 +1,40 @@
+import org.jgrapht.Graph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NeighborsBFS<V,E> {
+
+        private final Graph<V,E> graph;
+        private final V start;
+        private final BreadthFirstIterator<V,E> iterator;
+        private final Set<V> neighbors;
+        private final int depth;
+
+        public NeighborsBFS(Graph<V,E> graph, V start, int numNeighbors) {
+            this.graph = graph;
+            this.start = start;
+
+            iterator = new BreadthFirstIterator<>(graph, start);
+            Set<V> temp = new HashSet<>();
+
+            V current = start;
+            for (int i = 0; i < numNeighbors; i++) {
+                current = iterator.next();
+                temp.add(current);
+            }
+
+            depth = iterator.getDepth(current);
+            neighbors = temp;
+        }
+
+        public Set<V> getNeighbors() {
+            return neighbors;
+        }
+
+        public int getDepth() {
+            return depth;
+        }
+}
+
